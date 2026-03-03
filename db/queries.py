3fdf582e853eb3c72095f163be2bcaa7a6b06c44
Maierhofer Kilian <kilian.maierhofer@htlwy.at>
2026-03-03 09:38:13 +0100

    remove s from function

--- db/queries.py
+++ db/queries.py
@@ -43,7 +43,7 @@ def create_selected_from_users_table(filter_class: str, filter_name: str):
     return rows
 
 #   get_entity_instance_by_id(IN p_entity_instance_id INT, IN p_isEvent BOOLEAN)
-def get_entity_instances_by_id(id: int, is_event: bool = True):
+def get_entity_instance_by_id(id: int, is_event: bool = True):
     conn = get_connection()
     cur = conn.cursor(dictionary=True)
     cur.execute("CALL get_entity_instance_by_id(?,?)", (id, is_event))
